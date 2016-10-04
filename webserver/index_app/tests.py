c25bc1112ab3ef364d677c32b94779bc34fd580b
Jinyi Zhang <zhan1182@purdue.edu>
2016-10-04 14:37:14 -0400

    add static analysis

--- webserver/index_app/tests.py
+++ webserver/index_app/tests.py
@@ -1,10 +1,49 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-from pylint.lint import Run
+#run pylint on the given file
+from pylint import lint
+from pylint.reporters.text import TextReporter
+
+import os
 # Create your tests here.
+
+class WritableObject(object):
+    "dummy output stream for pylint"
+    def __init__(self):
+        self.content = []
+    def write(self, st):
+        "dummy write"
+        self.content.append(st)
+    def read(self):
+        "dummy read"
+        return self.content
+    def reset(self):
+        self.content = []
+
 
 class StaticAnalysis(TestCase):
 
-    def test_static_analysis(self):
-        pass
+    def get_analysis_score(self, pylint_output):
+        last_line = filter(None, [s.strip() for s in pylint_output.read()])[-1]
+        score = float(last_line.split()[6].split('/')[0])
 
+        return score
+
+    def test_static_analysis(self):
+        pylint_output = WritableObject()
+
+        # lint.Run(['index_app/models.py'])
+        files = ['index_app/models.py', 'index_app/views.py']
+
+        for filename in files:
+            pylint_output.reset()
+            lint.Run([filename], reporter=TextReporter(pylint_output), exit=False)
+            score = self.get_analysis_score(pylint_output)
+            self.assertEqual(score, abs(score))
+
+
+class ViewsTester(TestCase):
+
+    def test_index_view(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
