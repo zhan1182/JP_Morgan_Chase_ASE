45380fbe7f426e047dc6eedd39c5cc5ab544b634
Jinyi Zhang <zhan1182@purdue.edu>
2016-10-04 13:55:59 -0400

    init index app

---
+++ webserver/index_app/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+from pylint.lint import Run
+# Create your tests here.
+
+class StaticAnalysis(TestCase):
+
+    def test_static_analysis(self):
+        pass
+
--- webserver/webserver/settings.py
+++ webserver/webserver/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'trading_system.apps.TradingSystemConfig',
+    'index_app.apps.IndexAppConfig',
 ]
 
 MIDDLEWARE = [
