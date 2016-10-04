87acca23e9361b18cd20df0a8d2486bbab85ab4d
Jinyi Zhang <zhan1182@purdue.edu>
2016-10-04 14:43:39 -0400

    static analysis tester finished

--- webserver/index_app/tests.py
+++ webserver/index_app/tests.py
@@ -4,7 +4,6 @@ from django.test import TestCase, Client
 from pylint import lint
 from pylint.reporters.text import TextReporter
 
-import os
 # Create your tests here.
 
 class WritableObject(object):
@@ -26,20 +25,19 @@ class StaticAnalysis(TestCase):
     def get_analysis_score(self, pylint_output):
         last_line = filter(None, [s.strip() for s in pylint_output.read()])[-1]
         score = float(last_line.split()[6].split('/')[0])
-
         return score
 
-    def test_static_analysis(self):
+    def static_analysis_run(self, files):
         pylint_output = WritableObject()
-
-        # lint.Run(['index_app/models.py'])
-        files = ['index_app/models.py', 'index_app/views.py']
-
         for filename in files:
             pylint_output.reset()
             lint.Run([filename], reporter=TextReporter(pylint_output), exit=False)
             score = self.get_analysis_score(pylint_output)
             self.assertEqual(score, abs(score))
+
+    def test_static_analysis(self):
+        files = ['index_app/models.py', 'index_app/views.py']
+        self.static_analysis_run(files)
 
 
 class ViewsTester(TestCase):
--- webserver/index_app/views.py
+++ webserver/index_app/views.py
@@ -4,5 +4,4 @@ from django.http import HttpResponse
 
 def index(request):
 
-	return HttpResponse('hello')
 	return render(request, 'index/index.html')
